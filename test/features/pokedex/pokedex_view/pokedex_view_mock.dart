2dbe3483984f8666d046c27bb127477f393958fe
Miguel Caraballo de la Rosa <mcaraballodelarosa@outlook.com>
2022-05-09 23:28:09 +0200

    Undo mock

--- test/features/pokedex/pokedex_view/pokedex_view_mock.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:pokedex/features/pokedex/cubit/pokedex_cubit.dart';
-
-class PokedexCubitMock extends Mock implements PokedexCubit {}
